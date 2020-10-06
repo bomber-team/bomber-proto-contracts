2e3f33e0af53e5bb81104919cadfd146c37283ca
GitHub Action <action@github.com>
2020-10-06 12:53:12 +0000

    generate golang contracts

--- golang/rest_contracts/script.pb.go
+++ golang/rest_contracts/script.pb.go
@@ -23,13 +23,13 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type RestScript struct {
-	SchemeId             string   `protobuf:"bytes,1,opt,name=schemeId,proto3" json:"schemeId,omitempty"`
-	Address              string   `protobuf:"bytes,2,opt,name=address,proto3" json:"address,omitempty"`
-	RequestMethod        string   `protobuf:"bytes,3,opt,name=requestMethod,proto3" json:"requestMethod,omitempty"`
-	Rps                  int64    `protobuf:"varint,4,opt,name=rps,proto3" json:"rps,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	SchemeId             string               `protobuf:"bytes,1,opt,name=schemeId,proto3" json:"schemeId,omitempty"`
+	Address              string               `protobuf:"bytes,2,opt,name=address,proto3" json:"address,omitempty"`
+	RequestMethod        string               `protobuf:"bytes,3,opt,name=requestMethod,proto3" json:"requestMethod,omitempty"`
+	Config               *ConfigurationScript `protobuf:"bytes,4,opt,name=config,proto3" json:"config,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
+	XXX_unrecognized     []byte               `json:"-"`
+	XXX_sizecache        int32                `json:"-"`
 }
 
 func (m *RestScript) Reset()         { *m = RestScript{} }
@@ -86,15 +86,71 @@ func (m *RestScript) GetRequestMethod() string {
 	return ""
 }
 
-func (m *RestScript) GetRps() int64 {
+func (m *RestScript) GetConfig() *ConfigurationScript {
+	if m != nil {
+		return m.Config
+	}
+	return nil
+}
+
+type ConfigurationScript struct {
+	Rps                  int64    `protobuf:"varint,1,opt,name=rps,proto3" json:"rps,omitempty"`
+	Time                 int64    `protobuf:"varint,2,opt,name=time,proto3" json:"time,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ConfigurationScript) Reset()         { *m = ConfigurationScript{} }
+func (m *ConfigurationScript) String() string { return proto.CompactTextString(m) }
+func (*ConfigurationScript) ProtoMessage()    {}
+func (*ConfigurationScript) Descriptor() ([]byte, []int) {
+	return fileDescriptor_91f6f3ec99002da4, []int{1}
+}
+func (m *ConfigurationScript) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *ConfigurationScript) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_ConfigurationScript.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *ConfigurationScript) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ConfigurationScript.Merge(m, src)
+}
+func (m *ConfigurationScript) XXX_Size() int {
+	return m.Size()
+}
+func (m *ConfigurationScript) XXX_DiscardUnknown() {
+	xxx_messageInfo_ConfigurationScript.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ConfigurationScript proto.InternalMessageInfo
+
+func (m *ConfigurationScript) GetRps() int64 {
 	if m != nil {
 		return m.Rps
 	}
 	return 0
 }
 
+func (m *ConfigurationScript) GetTime() int64 {
+	if m != nil {
+		return m.Time
+	}
+	return 0
+}
+
 func init() {
 	proto.RegisterType((*RestScript)(nil), "contracts.rest_contracts.RestScript")
+	proto.RegisterType((*ConfigurationScript)(nil), "contracts.rest_contracts.ConfigurationScript")
 }
 
 func init() {
@@ -102,21 +158,24 @@ func init() {
 }
 
 var fileDescriptor_91f6f3ec99002da4 = []byte{
-	// 221 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x90, 0xbd, 0x4a, 0x03, 0x41,
-	0x10, 0xc7, 0x59, 0x4f, 0xfc, 0x18, 0x10, 0x64, 0xab, 0xd5, 0xe2, 0x08, 0xa2, 0x90, 0x6a, 0x53,
-	0xf8, 0x06, 0x5a, 0x59, 0xd8, 0x9c, 0x9d, 0x4d, 0xb8, 0xdb, 0x1d, 0xf6, 0x02, 0xd9, 0xcc, 0x39,
-	0x33, 0x69, 0x7c, 0x42, 0x4b, 0x1f, 0x41, 0xee, 0x49, 0x24, 0x2b, 0x39, 0x8d, 0xdd, 0xff, 0xe3,
-	0x57, 0xcc, 0x7f, 0xe0, 0x2e, 0xd0, 0x46, 0xb9, 0x0d, 0x2a, 0x0b, 0x46, 0xd1, 0xe5, 0xaf, 0x95,
-	0xc0, 0xab, 0x41, 0xfd, 0xc0, 0xa4, 0x64, 0xdd, 0x94, 0xfb, 0x43, 0xec, 0xe6, 0x1d, 0xa0, 0x41,
-	0xd1, 0x97, 0x42, 0xdb, 0x6b, 0x38, 0x93, 0xd0, 0x63, 0xc6, 0xa7, 0xe8, 0xcc, 0xcc, 0xcc, 0xcf,
-	0x9b, 0xc9, 0x5b, 0x07, 0xa7, 0x6d, 0x8c, 0x8c, 0x22, 0xee, 0xa8, 0x54, 0x7b, 0x6b, 0x6f, 0xe1,
-	0x82, 0xf1, 0x6d, 0x8b, 0xa2, 0xcf, 0xa8, 0x3d, 0x45, 0x57, 0x95, 0xfe, 0x30, 0xb4, 0x97, 0x50,
-	0xf1, 0x20, 0xee, 0x78, 0x66, 0xe6, 0x55, 0xb3, 0x93, 0x0f, 0xeb, 0x8f, 0xb1, 0x36, 0x9f, 0x63,
-	0x6d, 0xbe, 0xc6, 0xda, 0xc0, 0x15, 0x71, 0xf2, 0x1d, 0xe5, 0x0e, 0xd9, 0x2b, 0xb6, 0xd9, 0x4f,
-	0x47, 0xbe, 0x3e, 0xa6, 0x95, 0xf6, 0xdb, 0xce, 0x07, 0xca, 0x8b, 0x1f, 0x62, 0xb9, 0x23, 0xf6,
-	0xba, 0x6c, 0xfc, 0x33, 0x3d, 0xd1, 0xba, 0xdd, 0xa4, 0x7f, 0x0f, 0xe9, 0x4e, 0x0a, 0x76, 0xff,
-	0x1d, 0x00, 0x00, 0xff, 0xff, 0x5c, 0xc8, 0x41, 0xad, 0x33, 0x01, 0x00, 0x00,
+	// 270 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x91, 0xc1, 0x4a, 0xc3, 0x40,
+	0x10, 0x86, 0x59, 0x53, 0xaa, 0x8e, 0x08, 0xb2, 0x5e, 0x56, 0x0f, 0xa1, 0x14, 0x85, 0x5e, 0xdc,
+	0x82, 0x1e, 0xbd, 0x59, 0x3c, 0x78, 0xf0, 0x12, 0x6f, 0x5e, 0x4a, 0xb2, 0x19, 0x93, 0x40, 0x93,
+	0x89, 0xb3, 0x93, 0xe7, 0xf2, 0x35, 0x3c, 0xfa, 0x08, 0x92, 0x27, 0x91, 0x6e, 0x6d, 0x6b, 0x45,
+	0x6f, 0xf3, 0xcf, 0xff, 0x2d, 0x7c, 0xcc, 0xc2, 0xa5, 0xa3, 0x46, 0x38, 0x75, 0xe2, 0xa7, 0x8c,
+	0x5e, 0xe6, 0xdb, 0xe8, 0x1d, 0x57, 0xad, 0xd8, 0x96, 0x49, 0x48, 0x9b, 0xcd, 0xde, 0xee, 0x62,
+	0xe3, 0x37, 0x05, 0x90, 0xa0, 0x97, 0xa7, 0x80, 0xeb, 0x73, 0x38, 0xf0, 0xae, 0xc4, 0x1a, 0x1f,
+	0x72, 0xa3, 0x46, 0x6a, 0x72, 0x98, 0x6c, 0xb2, 0x36, 0xb0, 0x9f, 0xe6, 0x39, 0xa3, 0xf7, 0x66,
+	0x2f, 0x54, 0xeb, 0xa8, 0x2f, 0xe0, 0x98, 0xf1, 0xb5, 0x43, 0x2f, 0x8f, 0x28, 0x25, 0xe5, 0x26,
+	0x0a, 0xfd, 0xee, 0x52, 0xdf, 0xc3, 0xd0, 0x51, 0xf3, 0x52, 0x15, 0x66, 0x30, 0x52, 0x93, 0xa3,
+	0xeb, 0x2b, 0xfb, 0x9f, 0x95, 0x9d, 0x05, 0xae, 0xe3, 0x54, 0x2a, 0x6a, 0x56, 0x6a, 0xc9, 0xf7,
+	0xe3, 0xf1, 0x2d, 0x9c, 0xfe, 0x51, 0xeb, 0x13, 0x88, 0xb8, 0xf5, 0x41, 0x3a, 0x4a, 0x96, 0xa3,
+	0xd6, 0x30, 0x90, 0xaa, 0xc6, 0x20, 0x1b, 0x25, 0x61, 0xbe, 0x5b, 0xbc, 0xf7, 0xb1, 0xfa, 0xe8,
+	0x63, 0xf5, 0xd9, 0xc7, 0x0a, 0xce, 0x88, 0x0b, 0x9b, 0x51, 0x9d, 0x21, 0x5b, 0xc1, 0xb4, 0xde,
+	0x0a, 0x3d, 0xcf, 0x8a, 0x4a, 0xca, 0x2e, 0xb3, 0x8e, 0xea, 0xe9, 0x8a, 0x98, 0x2f, 0x89, 0xf5,
+	0x1c, 0xce, 0xfa, 0xe3, 0xda, 0x05, 0x2d, 0xd2, 0xa6, 0xf8, 0xf5, 0x07, 0xd9, 0x30, 0x60, 0x37,
+	0x5f, 0x01, 0x00, 0x00, 0xff, 0xff, 0x68, 0xb9, 0x5f, 0xa8, 0xa6, 0x01, 0x00, 0x00,
 }
 
 func (m *RestScript) Marshal() (dAtA []byte, err error) {
@@ -143,10 +202,17 @@ func (m *RestScript) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i -= len(m.XXX_unrecognized)
 		copy(dAtA[i:], m.XXX_unrecognized)
 	}
-	if m.Rps != 0 {
-		i = encodeVarintScript(dAtA, i, uint64(m.Rps))
+	if m.Config != nil {
+		{
+			size, err := m.Config.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintScript(dAtA, i, uint64(size))
+		}
 		i--
-		dAtA[i] = 0x20
+		dAtA[i] = 0x22
 	}
 	if len(m.RequestMethod) > 0 {
 		i -= len(m.RequestMethod)
@@ -168,6 +234,43 @@ func (m *RestScript) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i = encodeVarintScript(dAtA, i, uint64(len(m.SchemeId)))
 		i--
 		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *ConfigurationScript) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ConfigurationScript) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *ConfigurationScript) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	if m.Time != 0 {
+		i = encodeVarintScript(dAtA, i, uint64(m.Time))
+		i--
+		dAtA[i] = 0x10
+	}
+	if m.Rps != 0 {
+		i = encodeVarintScript(dAtA, i, uint64(m.Rps))
+		i--
+		dAtA[i] = 0x8
 	}
 	return len(dAtA) - i, nil
 }
@@ -201,8 +304,27 @@ func (m *RestScript) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovScript(uint64(l))
 	}
+	if m.Config != nil {
+		l = m.Config.Size()
+		n += 1 + l + sovScript(uint64(l))
+	}
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *ConfigurationScript) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
 	if m.Rps != 0 {
 		n += 1 + sovScript(uint64(m.Rps))
+	}
+	if m.Time != 0 {
+		n += 1 + sovScript(uint64(m.Time))
 	}
 	if m.XXX_unrecognized != nil {
 		n += len(m.XXX_unrecognized)
@@ -342,6 +464,96 @@ func (m *RestScript) Unmarshal(dAtA []byte) error {
 			m.RequestMethod = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 4:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Config", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowScript
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthScript
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthScript
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Config == nil {
+				m.Config = &ConfigurationScript{}
+			}
+			if err := m.Config.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipScript(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthScript
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthScript
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *ConfigurationScript) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowScript
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ConfigurationScript: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ConfigurationScript: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
 			if wireType != 0 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Rps", wireType)
 			}
@@ -356,6 +568,25 @@ func (m *RestScript) Unmarshal(dAtA []byte) error {
 				b := dAtA[iNdEx]
 				iNdEx++
 				m.Rps |= int64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Time", wireType)
+			}
+			m.Time = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowScript
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Time |= int64(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
